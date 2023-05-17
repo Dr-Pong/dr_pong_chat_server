9937636eaea66e4d97bdf550dc88c9641b7fcc92
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-17 11:35:50 +0900

    [Feat]AppModule추가#3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FrinedModule } from './frined/frined.module';
+import { FrinedModule } from './domain/frined/frined.module';
+import { UserModule } from './domain/user/user.module';
+import { ProfileImageModule } from './domain/profile-image/profile-image.module';
 
 @Module({
-  imports: [FrinedModule],
+  imports: [FrinedModule, UserModule, ProfileImageModule],
   controllers: [AppController],
   providers: [AppService],
 })
