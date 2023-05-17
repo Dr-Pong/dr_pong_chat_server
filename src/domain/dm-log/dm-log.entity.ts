91966a63cfa7f57b81e7d3f7637bf6ec82e11e0a
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-17 18:36:37 +0900

    [Feat] dm-log entity 추가 #2

---
+++ src/domain/dm-log/dm-log.entity.ts
@@ -0,0 +1,26 @@
+import { BaseTimeEntity } from 'src/global/base-entity/base-time.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../user/user.entity';
+
+@Entity()
+export class DmLog extends BaseTimeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: 'sender' })
+  sender: User;
+
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: 'receiver' })
+  receiver: User;
+
+  @Column({ name: 'log', nullable: false })
+  log: string;
+}
