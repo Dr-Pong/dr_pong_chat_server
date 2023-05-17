48f95565be9706b5ff007c1b95593f832028971f
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-17 17:51:48 +0900

    [Feat] block 엔티티추가#4

---
+++ src/domain/block/block.entity.ts
@@ -0,0 +1,17 @@
+import { BaseTimeEntity } from 'src/global/base-entity/base-time.entity';
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../user/user.entity';
+
+@Entity()
+export class Block extends BaseTimeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: 'blocked_id' })
+  blockedUser: User;
+}
