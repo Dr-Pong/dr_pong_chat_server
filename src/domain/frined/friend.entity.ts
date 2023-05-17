d8785ce3d5da0a9d68ab7e0f9111f3306b669950
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-17 11:34:44 +0900

    [Feat]경로 변경#3

---
+++ src/domain/frined/friend.entity.ts
@@ -0,0 +1,17 @@
+import { BaseTimeEntity } from 'src/global/base-entity/base-time.entity';
+import { User } from 'src/user/user.entity';
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Friend extends BaseTimeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: 'friend_id' })
+  friend: User;
+}
