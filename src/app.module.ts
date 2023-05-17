8593faf581efe551fa7d0a26440d35d59b94c863
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-17 11:14:24 +0900

    [Feat]Friend user없이 추가#3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FrinedModule } from './frined/frined.module';
 
 @Module({
-  imports: [],
+  imports: [FrinedModule],
   controllers: [AppController],
   providers: [AppService],
 })
