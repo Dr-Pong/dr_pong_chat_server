d34f6f91ba55612d2a177321ebc752f3df845ccc
nheo9143 <nheo@student.42seoul.kr>
2023-05-18 18:59:55 +0900

    [Feat] 유저, 채널 상태 관리용 type 추가 #19

---
+++ src/global/type/type.channel.ts
@@ -0,0 +1,5 @@
+export const CHANNEL_PUBLIC = 'public' as const;
+export const CHANNEL_PROTECTED = 'protected' as const;
+export const CHANNEL_PRIVATE = 'private' as const;
+
+export type ChannelType = 'public' | 'protected' | 'private';
