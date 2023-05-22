8967c784e7892a19da75749ac0be36432a14ccd4
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-22 12:26:09 +0900

    [Feat]친구 요청 목록 조회 dto 생성 #11

---
+++ src/domain/frined/dto/get.user.peding.friends.dto.ts
@@ -0,0 +1,3 @@
+export class GetUserPendingFriendsDto {
+  userId: number;
+}
---
+++ src/domain/frined/dto/user.pending.friends.dto.ts
@@ -0,0 +1,5 @@
+import { user } from './user.friends.dto';
+
+export class UserPendingFriendsDto {
+  users: user[];
+}
