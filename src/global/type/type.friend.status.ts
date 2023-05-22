d2c4156860e8f6b2dec2bf810f7b29679c757dcf
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-22 12:00:04 +0900

    [Fix]type에 delete추가

--- src/global/type/type.friend.status.ts
+++ src/global/type/type.friend.status.ts
@@ -1,4 +1,5 @@
 export const FRIENDSTATUS_FRIEND = 'friend' as const;
 export const FRIENDSTATUS_REQUESTING = 'requesting' as const;
+export const FRIENDSTATUS_DELETED = 'deleted' as const;
 
-export type FriendType = 'friend' | 'requesting';
+export type FriendType = 'friend' | 'requesting' | 'deleted';
