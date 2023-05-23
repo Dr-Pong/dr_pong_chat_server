87bebc99aaf965ed742994519c0564a568ea36f0
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-23 15:18:10 +0900

    [Fix] 친구요청 조회 테스트 파일 #11

--- src/domain/frined/friend.service.spec.ts
+++ src/domain/frined/friend.service.spec.ts
@@ -135,7 +135,7 @@ describe('FriendService', () => {
       });
     });
     describe('친구요청', () => {
-      it('[Valid Case]친구요청', async () => {
+      it('[Valid Case]유효한 닉네임 친구요청', async () => {
         const userFriendsRequestDto: PostUserFriendsRequestDto = {
           userId: testData.users[0].id,
           friendId: testData.users[1].id,
@@ -152,6 +152,26 @@ describe('FriendService', () => {
         });
 
         expect(friendRequest.status).toBe(FRIENDSTATUS_REQUESTING);
+      });
+
+      it('[Valid Case]이미 친구인 유저에게 친구요청(백에서 씹기)', async () => {
+        await testData.createUserFriends();
+        const userFriendsRequestDto: PostUserFriendsRequestDto = {
+          userId: testData.users[0].id,
+          friendId: testData.users[1].id,
+        };
+
+        await service.postUserFriendsRequestByNickname(userFriendsRequestDto);
+
+        const friendRequest: Friend = await friendRepository.findOne({
+          where: {
+            user: { id: testData.users[0].id },
+            friend: { id: testData.users[1].id },
+            status: Not(FRIENDSTATUS_DELETED),
+          },
+        });
+
+        expect(friendRequest.status).toBe(FRIENDSTATUS_FRIEND);
       });
     });
     describe('친구요청 목록', () => {
