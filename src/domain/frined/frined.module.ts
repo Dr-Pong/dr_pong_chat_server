7a2f2ec8ef23f59afe6c08ec1494f1acbecc4fd4
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-23 16:52:10 +0900

    [Fix] 사용하지 않는 이미지 레포 모듈에서 제거 #11

--- src/domain/frined/frined.module.ts
+++ src/domain/frined/frined.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { Friend } from './friend.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FriendService } from './friend.service';
-import { ProfileImageRepository } from '../profile-image/profile-image.repository';
-import { ProfileImage } from '../profile-image/profile-image.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Friend, ProfileImage])],
-  providers: [FriendService, ProfileImageRepository],
+  imports: [TypeOrmModule.forFeature([Friend])],
+  providers: [FriendService],
   exports: [FriendService],
   controllers: [],
 })
