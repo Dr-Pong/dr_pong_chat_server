d5b38f3e6faf398514aed447eb1afd5292de17f4
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-24 10:47:29 +0900

    [Feat] DM 전용 테스트 서비스, 모듈 파일 생성 (친구 테스트 가져온것) #23

---
+++ src/domain/dm-log/test/test.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { TestService } from './test.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/domain/user/user.entity';
+import { Block } from 'src/domain/block/block.entity';
+import { ProfileImage } from 'src/domain/profile-image/profile-image.entity';
+import { ProfileImageRepository } from 'src/domain/profile-image/profile-image.repository';
+import { DmLog } from '../dm-log.entity';
+import { Friend } from 'src/domain/frined/friend.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      Friend,
+      Block,
+      ProfileImageRepository,
+      ProfileImage,
+      DmLog,
+    ]),
+  ],
+  providers: [TestService],
+  exports: [
+    TestService,
+    TypeOrmModule.forFeature([
+      User,
+      Friend,
+      Block,
+      ProfileImage,
+      ProfileImageRepository,
+      DmLog,
+    ]),
+  ],
+})
+export class TestModule {}
