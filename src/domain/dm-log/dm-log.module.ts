9eaa1b39e7e3b448017976e086c382a25760ef4f
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-24 10:10:56 +0900

    [Feat] dmlog module 추가

--- src/domain/dm-log/dm-log.module.ts
+++ src/domain/dm-log/dm-log.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DmLog } from './dm-log.entity';
+import { DmLogService } from './dm-log.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([DmLog])],
+  providers: [DmLogService],
+  exports: [DmLogService],
+  controllers: [],
+})
 export class DmLogModule {}
