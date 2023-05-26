10672d9b7bb61967602250be1e752b0c63c10ed8
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-26 10:40:53 +0900

    [Fix] Frined 를 Friend로 파일이름 수정 이름만 바뀌였어요 #26

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FrinedModule } from './domain/frined/frined.module';
+import { FrinedModule } from './domain/friend/frined.module';
 import { UserModule } from './domain/user/user.module';
 import { ProfileImageModule } from './domain/profile-image/profile-image.module';
 import { BlockModule } from './domain/block/block.module';
