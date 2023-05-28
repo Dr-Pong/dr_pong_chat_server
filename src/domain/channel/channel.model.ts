7bceea411e15a3090f6e11a0106560638b53c93b
nheo9143 <nheo@student.42seoul.kr>
2023-05-28 16:50:58 +0900

    [Fix] id를 string으로 변경, 내부 변수를 Map

--- src/domain/channel/channel.model.ts
+++ src/domain/channel/channel.model.ts
@@ -1,14 +1,30 @@
 import { ChannelType } from 'src/global/type/type.channel';
+import { Channel } from './channel.entity';
 
 export class ChannelModel {
-  id: number;
+  id: string;
   name: string;
-  users: Set<number> = new Set();
+  users: Map<number, number>;
   type: ChannelType;
   password: string;
   ownerId: number;
-  muteList: number[];
-  banList: number[];
-  adminList: number[];
+  muteList: Map<number, number>;
+  banList: Map<number, number>;
+  adminList: Map<number, number>;
   maxHeadCount: number;
+
+  static fromEntity(channel: Channel): ChannelModel {
+    const channelModel = new ChannelModel();
+    channelModel.id = channel.id;
+    channelModel.name = channel.name;
+    channelModel.users = new Map();
+    channelModel.type = channel.type;
+    channelModel.password = channel.password;
+    channelModel.ownerId = channel.operator.id;
+    channelModel.muteList = new Map();
+    channelModel.banList = new Map();
+    channelModel.adminList = new Map();
+    channelModel.maxHeadCount = channel.maxHeadCount;
+    return channelModel;
+  }
 }
