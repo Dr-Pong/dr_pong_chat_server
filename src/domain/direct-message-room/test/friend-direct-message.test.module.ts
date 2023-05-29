11d1aa9da5c018932c7e89acc84e1bfb8b9fc8a4
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-29 17:43:54 +0900

    [Fix] test 파일  이름 변경 #26
    -friend dircet message 에서 Direct Message Room으로 변경

---
+++ src/domain/direct-message-room/test/friend-direct-message.test.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { FriendDirectMessageTestService } from './friend-direct-message.test.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/domain/user/user.entity';
+import { Block } from 'src/domain/block/block.entity';
+import { ProfileImage } from 'src/domain/profile-image/profile-image.entity';
+import { ProfileImageRepository } from 'src/domain/profile-image/profile-image.repository';
+import { Friend } from 'src/domain/friend/friend.entity';
+import { DirectMessage } from 'src/domain/direct-message/direct-message.entity';
+import { DirectMessageRoom } from '../direct-message-room.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      Friend,
+      Block,
+      ProfileImageRepository,
+      ProfileImage,
+      DirectMessage,
+      DirectMessageRoom,
+    ]),
+  ],
+  providers: [FriendDirectMessageTestService],
+  exports: [
+    FriendDirectMessageTestService,
+    TypeOrmModule.forFeature([
+      User,
+      Friend,
+      Block,
+      ProfileImage,
+      ProfileImageRepository,
+      DirectMessage,
+      DirectMessageRoom,
+    ]),
+  ],
+})
+export class TestModule {}
