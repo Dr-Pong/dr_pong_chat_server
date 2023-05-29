221778573b02c224afb3dbb506306dfb302a383b
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-29 17:43:32 +0900

    [Fix] Dto 이름 변경 #26
    -friend dircet message 에서 Direct Message Room으로 변경

---
+++ src/domain/direct-message-room/dto/delete.direct-message-room.dto.ts
@@ -0,0 +1,4 @@
+export class DeleteDirectMessageRoomDto {
+  userId: number;
+  friendId: number;
+}
---
+++ src/domain/direct-message-room/dto/get.direct-message-rooms.notification.response.dto.ts
@@ -0,0 +1,3 @@
+export class GetDirectMessageRoomsNotificationResponseDto {
+  hasNewChat: boolean;
+}
