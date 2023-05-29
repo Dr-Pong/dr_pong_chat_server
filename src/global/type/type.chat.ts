acd43c1c46c0a1f4e0c45515bb89fd38ed962356
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-29 13:31:58 +0900

    [Fix] type is_chat_on 삭제

--- src/global/type/type.chat.ts
+++
@@ -1,6 +0,0 @@
-export const CHATINGTYPE_INCHAT = 'in-chat' as const;
-export const CHATINGTYPE_OUTCHAT = 'out-chat' as const;
-export const CHATINGTYPE_DELETE = 'delete' as const;
-export const CHATINGTYPE_BLOCK = 'block' as const;
-
-export type ChatingType = 'in-chat' | 'out-chat' | 'delete' | 'block';
