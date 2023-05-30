681faac8d31f0dcb3d4077ff01712f5b8c76e014
nheo9143 <nheo@student.42seoul.kr>
2023-05-30 16:54:38 +0900

    [Feat] 채널 목록 조회용 레포지토리 함수 구현 #5

--- src/domain/channel/channel.repository.ts
+++ src/domain/channel/channel.repository.ts
@@ -2,56 +2,93 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Channel } from './channel.entity';
 import { Like, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { Page } from 'src/global/utils/page';
+import { FindChannelPageDto } from '../channel-user/dto/find.channel.page.dto';
 
 @Injectable()
 export class ChannelRepository {
   constructor(
     @InjectRepository(Channel)
-    private readonly channelRepository: Repository<Channel>,
+    private readonly repository: Repository<Channel>,
   ) {}
 
-  async findChannelByPagesOrderByCreateAt(
-    page: number,
-    limit: number,
-    keyword: string,
-  ) {
-    return await this.channelRepository.find({
+  async findByChannelName(name: string): Promise<Channel> {
+    return await this.repository.findOne({
       where: {
-        name: Like('%' + keyword + '%'),
+        name: name,
         isDeleted: false,
       },
-      skip: page * limit,
-      take: limit,
+    });
+  }
+
+  async findChannelByPagesOrderByCreateAt(
+    findDto: FindChannelPageDto,
+  ): Promise<Page<Channel[]>> {
+    const totalChannels: number = await this.repository.count({
+      where: {
+        name: Like('%' + findDto.keyword + '%'),
+        isDeleted: false,
+      },
+    });
+
+    const channels: Channel[] = await this.repository.find({
+      where: {
+        name: Like('%' + findDto.keyword + '%'),
+        isDeleted: false,
+      },
+      skip: (findDto.page - 1) * findDto.count,
+      take: findDto.count,
       order: {
         createdAt: 'DESC',
       },
     });
+
+    const page: Page<Channel[]> = new Page();
+    page.content = channels;
+    page.totalPage = totalChannels / findDto.count + 1;
+    page.currentPage =
+      findDto.page > page.totalPage ? page.totalPage : findDto.page;
+
+    return page;
   }
 
-  async findChannelByPagesOrderByPopular(
-    page: number,
-    limit: number,
-    keyword: string,
-  ) {
-    return await this.channelRepository.find({
+  async findChannelByPagesOrderByHeadCount(
+    findDto: FindChannelPageDto,
+  ): Promise<Page<Channel[]>> {
+    const totalChannels: number = await this.repository.count({
       where: {
-        name: Like('%' + keyword + '%'),
+        name: Like('%' + findDto.keyword + '%'),
         isDeleted: false,
       },
-      skip: page * limit,
-      take: limit,
+    });
+
+    const channels: Channel[] = await this.repository.find({
+      where: {
+        name: Like('%' + findDto.keyword + '%'),
+        isDeleted: false,
+      },
+      skip: (findDto.page - 1) * findDto.count,
+      take: findDto.count,
       order: {
         headCount: 'DESC',
       },
     });
+
+    const page: Page<Channel[]> = new Page();
+    page.content = channels;
+    page.totalPage = totalChannels / findDto.count + 1;
+    page.currentPage =
+      findDto.page > page.totalPage ? page.totalPage : findDto.page;
+
+    return page;
   }
 
   async saveChannel(channel: Channel): Promise<Channel> {
-    return await this.channelRepository.save(channel);
+    return await this.repository.save(channel);
   }
 
   async deleteChannel(channel: Channel): Promise<void> {
     channel.isDeleted = true;
-    await this.channelRepository.save(channel);
+    await this.repository.save(channel);
   }
 }
