f6f58b4aa5d00f2ad9ef0938063183fbcf891c57
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-30 15:37:36 +0900

    [Feat] 친구목록을 가져오는 서비스 로직 #9

--- src/domain/friend/friend.service.ts
+++ src/domain/friend/friend.service.ts
@@ -1,7 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { ProfileImageRepository } from '../profile-image/profile-image.repository';
+import { FriendRepository } from './friend.repository';
+import { UserFactory } from '../user/user.factory';
+import { GetUserFriendDto } from './dto/get.user.friend.dto';
+import { UserFriendsDto } from './dto/user.friends.dto';
+import { Friend } from './friend.entity';
 
 @Injectable()
 export class FriendService {
-  constructor() {}
+  constructor(
+    private friendRepository: FriendRepository,
+    private userFactory: UserFactory,
+  ) {}
+
+  async getUserFriends(getDto: GetUserFriendDto): Promise<UserFriendsDto> {
+    const userFriends: Friend[] = await this.friendRepository.findFriendsById(
+      getDto.userId,
+    );
+    const friends = userFriends.map((friend) => {
+      return {
+        nickname: friend.friend.nickname,
+        imgUrl: friend.friend.image.url,
+      };
+    });
+
+    const responseDto: UserFriendsDto = {
+      friends: friends,
+    };
+
+    return responseDto;
+  }
 }
