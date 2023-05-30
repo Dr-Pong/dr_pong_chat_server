b31244787a19a5d06e4284b0fdb0d0d108c090f1
nheo9143 <nheo@student.42seoul.kr>
2023-05-30 12:23:41 +0900

    [Feat] 채팅방 참여자 조회를 위한 서비스 추가 #5

--- src/domain/channel-user/channel-user.service.ts
+++ src/domain/channel-user/channel-user.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { ChannelUserRepository } from './channel-user.repository';
+import { ChannelRepository } from '../channel/channel.repository';
+import { Channel } from '../channel/channel.entity';
+import { ChannelFactory } from '../channel/channel.factory';
+import { ChannelModel } from '../channel/channel.model';
+import { UserModel } from '../user/user.model';
+import {
+  IsolationLevel,
+  Transactional,
+  runOnTransactionComplete,
+} from 'typeorm-transactional';
+import { UserFactory } from '../user/user.factory';
+import { ChannelUser } from './channel-user.entity';
+import { User } from '../user/user.entity';
+import {
+  CHANNEL_PRIVATE,
+  CHANNEL_PROTECTED,
+} from 'src/global/type/type.channel';
+import { ChannelJoinDto } from './dto/channel.join.dto';
+import { PostChannelAcceptInviteDto } from './dto/post.channel.accept.invite.dto';
+import { UpdateChannelHeadCountDto } from './dto/update.channel.headcount.dto';
+import { GetChannelParticipantsDto } from './dto/get.channel-participants.dto';
+import {
+  ChannelParticipantDto,
+  ChannelParticipantDtos,
+} from './dto/channel-participant.dto';
 
 @Injectable()
-export class ChannelUserService {}
+export class ChannelUserService {
+  constructor(
+    private readonly channelUserRepository: ChannelUserRepository,
+    private readonly channelRepository: ChannelRepository,
+    private readonly userRepository: UserRepository,
+    private readonly messageRepository: ChannelMessageRepository,
+    private readonly channelFactory: ChannelFactory,
+    private readonly userFactory: UserFactory,
+  ) {}
+
+  getChannelParticipants(
+    getDto: GetChannelParticipantsDto,
+  ): ChannelParticipantDtos {
+    const channel: ChannelModel = this.channelFactory.findChannelById(
+      getDto.channelId,
+    );
+    const users: UserModel[] = this.channelFactory.getUsers(channel);
+
+    const responseDto: ChannelParticipantDtos = new ChannelParticipantDtos();
+    users.map((user) => {
+      if (user.id === getDto.userId) {
+        responseDto.me = ChannelParticipantDto.fromModel(user);
+      } else {
+        responseDto.participants.push(ChannelParticipantDto.fromModel(user));
+      }
+    });
+    return responseDto;
+  }
+}
