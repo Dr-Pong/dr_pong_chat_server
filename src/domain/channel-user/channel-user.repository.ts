f81919a9eb851fb0ff521dcd6acf1959df865195
nheo9143 <nheo@student.42seoul.kr>
2023-06-01 14:02:12 +0900

    [Feat] 채널 생성용 repository 함수 구현 #5

--- src/domain/channel-user/channel-user.repository.ts
+++ src/domain/channel-user/channel-user.repository.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ChannelUser } from './channel-user.entity';
+import { User } from '../user/user.entity';
+import { Channel } from '../channel/channel.entity';
 
 @Injectable()
 export class ChannelUserRepository {
@@ -9,4 +11,20 @@ export class ChannelUserRepository {
     @InjectRepository(ChannelUser)
     private readonly userChannelRepository: Repository<ChannelUser>,
   ) {}
+
+  async findByUserIdAndNotDeleted(userId: number): Promise<ChannelUser> {
+    return await this.userChannelRepository.findOne({
+      where: {
+        user: { id: userId },
+        isDeleted: false,
+      },
+    });
+  }
+
+  async saveChannelUser(channel: Channel, user: User): Promise<ChannelUser> {
+    return await this.userChannelRepository.save({
+      channel: channel,
+      user: user,
+    });
+  }
 }
