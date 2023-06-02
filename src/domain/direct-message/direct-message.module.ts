af57dd30427b9930ba6ec7c6a728ba7f5b4500cc
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-02 12:39:36 +0900

    [Fix] Friend 모듈 추가 #24

--- src/domain/direct-message/direct-message.module.ts
+++ src/domain/direct-message/direct-message.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DirectMessage } from './direct-message.entity';
 import { DirectMessageService } from './direct-message.service';
 import { DirectMessageRepository } from './direct-message.repository';
+import { FriendRepository } from '../friend/friend.repository';
+import { Friend } from '../friend/friend.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DirectMessage])],
-  providers: [DirectMessageService, DirectMessageRepository],
+  imports: [TypeOrmModule.forFeature([DirectMessage, Friend])],
+  providers: [DirectMessageService, DirectMessageRepository, FriendRepository],
   exports: [DirectMessageService],
   controllers: [],
 })
