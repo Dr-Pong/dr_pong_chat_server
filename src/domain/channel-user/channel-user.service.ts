4b99e0b05d0c1cc1880cd343f8d8898e36c6dbf7
nheo9143 <nheo@student.42seoul.kr>
2023-06-02 12:42:18 +0900

    [Feat] 서비스마다 주석 추가 #5

--- src/domain/channel-user/channel-user.service.ts
+++ src/domain/channel-user/channel-user.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ChannelUserRepository } from './channel-user.repository';
 import { ChannelRepository } from '../channel/channel.repository';
 import { Channel } from '../channel/channel.entity';
@@ -50,6 +46,7 @@ import {
 } from 'src/global/type/type.join.channel';
 import { ChannelExitDto } from './dto/channel.exit.dto';
 import { DeleteChannelUserDto } from './dto/delete.channel.user.dto';
+import { ChannelMessageRepository } from '../channel-message/channel-message.repository';
 
 @Injectable()
 export class ChannelUserService {
@@ -61,6 +58,10 @@ export class ChannelUserService {
     private readonly userFactory: UserFactory,
   ) {}
 
+  /**
+   * 채널의 참여자 목록을 조회하는 함수
+   * Factory를 사용하기 때문에 비동기로 처리하지 않는다
+   */
   getChannelParticipants(
     getDto: GetChannelParticipantsDto,
   ): ChannelParticipantDtos {
@@ -81,6 +82,10 @@ export class ChannelUserService {
     return responseDto;
   }
 
+  /**
+   * 유저가 참여한 채널을 조회하는 함수
+   * Factory를 사용하기 때문에 비동기로 처리하지 않는다
+   */
   getChannelMy(getDto: GetChannelMyDto): ChannelMeDto {
     const user: UserModel = this.userFactory.findById(getDto.userId);
     const channel: ChannelModel = this.channelFactory.findChannelById(
@@ -90,18 +95,46 @@ export class ChannelUserService {
     return ChannelMeDto.fromModel(channel);
   }
 
+  /**
+   * 유저를 채널에 초대하는 함수
+   * UserModel의 inviteList에 추가해준다
+   * Factory를 사용하기 때문에 비동기로 처리하지 않는다
+   */
+  postInvite(postDto: PostInviteDto): void {
+    const channel: ChannelModel = this.channelFactory.channels.get(
+      postDto.channelId,
+    );
+    checkChannelExist(channel);
+
+    const host: UserModel = this.userFactory.findById(postDto.userId);
+    const target: UserModel = this.userFactory.findById(postDto.tragetId);
+    checkUserExist(host);
+    checkUserExist(target);
+
+    const invite: InviteModel = new InviteModel(
+      channel.id,
+      channel.name,
+      host.nickname,
+    );
+
+    this.userFactory.invite(target, invite);
+  }
+
+  /**
+   * 채널 목록을 조회하는 함수
+   * 생성 순, 참여자 순으로 정렬할 수 있다
+   */
   async getChannelPages(getDto: GetChannelPageDto): Promise<ChannelPageDtos> {
     let channels: Page<Channel[]>;
-    if (getDto.orderBy === 'createAt') {
-      channels = await this.channelRepository.findChannelByPagesOrderByCreateAt(
-        FindChannelPageDto.fromGetDto(getDto),
-      );
-    }
     if (getDto.orderBy === 'popular') {
       channels =
         await this.channelRepository.findChannelByPagesOrderByHeadCount(
           FindChannelPageDto.fromGetDto(getDto),
         );
+    } else {
+      channels = await this.channelRepository.findChannelByPagesOrderByCreateAt(
+        FindChannelPageDto.fromGetDto(getDto),
+      );
     }
 
     const responseDto: ChannelPageDtos = ChannelPageDtos.fromPage(channels);
@@ -109,6 +142,11 @@ export class ChannelUserService {
     return responseDto;
   }
 
+  /**
+   * 채널을 새로 생성하는 함수
+   * 채널 이름의 중복은 불가능
+   * 생성에 성공할 시 기존에 참여한 채널에서는 자동 퇴장처리된다
+   */
   @Transactional({ isolationLevel: IsolationLevel.REPEATABLE_READ })
   async postChannel(postDto: PostChannelDto): Promise<void> {
     const existChannel: Channel =
@@ -127,14 +165,14 @@ export class ChannelUserService {
       );
     }
 
-    const user: User = await this.userRepository.findById(postDto.userId);
     const newChannel: Channel = await this.channelRepository.saveChannel(
       SaveChannelDto.from(postDto),
     );
     await this.channelUserRepository.saveChannelUser(
-      new SaveChannelUserDto(user.id, newChannel.id),
+      new SaveChannelUserDto(postDto.userId, newChannel.id),
     );
 
+    /** 트랜잭션이 성공하면 Factory에도 결과를 반영한다 */
     runOnTransactionComplete(() => {
       const userModel: UserModel = this.userFactory.findById(postDto.userId);
       if (userModel.joinedChannel) {
@@ -150,6 +188,10 @@ export class ChannelUserService {
     });
   }
 
+  /**
+   * 유저를 채널에 입장시켜주는 함수
+   * 입장 성공 시 기존에 있던 채널에서는 자동 퇴장처리된다
+   */
   @Transactional({ isolationLevel: IsolationLevel.REPEATABLE_READ })
   async postChannelJoin(postDto: PostChannelJoinDto): Promise<void> {
     const existChannelUser: ChannelUser =
@@ -171,12 +213,13 @@ export class ChannelUserService {
       ),
     );
 
+    /** 트랜잭션이 성공하면 Factory에도 결과를 반영한다 */
     runOnTransactionComplete(() => {
       const userModel: UserModel = this.userFactory.findById(postDto.userId);
       if (userModel.joinedChannel) {
         this.channelFactory.leave(
           userModel,
-          this.channelFactory.channels.get(userModel.joinedChannel),
+          this.channelFactory.findChannelById(userModel.joinedChannel),
         );
       }
       this.channelFactory.join(
@@ -185,27 +228,10 @@ export class ChannelUserService {
       );
     });
   }
-
-  postInvite(postDto: PostInviteDto): void {
-    const channel: ChannelModel = this.channelFactory.channels.get(
-      postDto.channelId,
-    );
-    checkChannelExist(channel);
-
-    const host: UserModel = this.userFactory.findById(postDto.userId);
-    const target: UserModel = this.userFactory.findById(postDto.tragetId);
-    checkUserExist(host);
-    checkUserExist(target);
-
-    const invite: InviteModel = new InviteModel(
-      channel.id,
-      channel.name,
-      host.nickname,
-    );
-
-    this.userFactory.invite(target, invite);
-  }
-
+  /**
+   * 유저가 채널 초대를 수락하는 함수
+   * 입장 성공 시 기존에 있던 채널에서는 자동 퇴장처리된다
+   */
   @Transactional({ isolationLevel: IsolationLevel.REPEATABLE_READ })
   async postChannelAcceptInvite(
     postDto: PostChannelAcceptInviteDto,
@@ -232,6 +258,7 @@ export class ChannelUserService {
       ),
     );
 
+    /** 트랜잭션이 성공하면 Factory에도 결과를 반영한다 */
     runOnTransactionComplete(() => {
       const userModel: UserModel = this.userFactory.findById(postDto.userId);
       if (userModel.joinedChannel) {
@@ -250,6 +277,7 @@ export class ChannelUserService {
     });
   }
 
+  /** 유저가 채널에서 나가는 함수 */
   @Transactional({ isolationLevel: IsolationLevel.REPEATABLE_READ })
   async deleteChannelUser(deleteDto: DeleteChannelUserDto): Promise<void> {
     const channelUser: ChannelUser =
@@ -265,6 +293,7 @@ export class ChannelUserService {
       new ChannelExitDto(deleteDto.userId, channelUser.channel.id),
     );
 
+    /** 트랜잭션이 성공하면 Factory에도 결과를 반영한다 */
     runOnTransactionComplete(() => {
       const userModel: UserModel = this.userFactory.findById(deleteDto.userId);
       this.channelFactory.leave(
@@ -274,6 +303,11 @@ export class ChannelUserService {
     });
   }
 
+  /**
+   * 채널입장 처리하는 함수
+   * 에러 처리, DB save, update 처리용 함수
+   * 에러 케이스 - 채널이 없는 경우, 비밀번호가 틀린 경우, 인원이 꽉 찬 경우, 채널에서 BAN된 경우
+   * */
   private async joinChannel(dto: ChannelJoinDto): Promise<void> {
     await validateChannelJoin(
       dto,
@@ -289,6 +323,10 @@ export class ChannelUserService {
     );
   }
 
+  /**
+   * 채널퇴장 처리하는 함수
+   * 에러 처리, DB delete, update 처리용 함수
+   * */
   private async exitChannel(dto: ChannelExitDto): Promise<void> {
     await this.channelUserRepository.deleteChannelUser(
       dto.userId,
