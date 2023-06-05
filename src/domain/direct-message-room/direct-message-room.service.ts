de1eb6d6612d1c1c4a655698d8a21e1e7e59b9db
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-05 13:44:33 +0900

    [Feat] DmRoom get 하는 서비스로직 추가 #24-1

--- src/domain/direct-message-room/direct-message-room.service.ts
+++ src/domain/direct-message-room/direct-message-room.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { DirectMessageRepository } from '../direct-message/direct-message.repository';
+import { FriendRepository } from '../friend/friend.repository';
+import { DirectMessageRoomRepository } from './direct-message-room.repository';
+import { IsolationLevel, Transactional } from 'typeorm-transactional';
+import { GetDirectMessageRoomsDto } from './dto/get.direct-message-rooms.dto';
+import {
+  DirectMessageRoomInfoDto,
+  DirectMessageRoomsDto,
+} from './dto/direct-message-rooms.dto';
+import { DirectMessageRoom } from './direct-message-room.entity';
 
 @Injectable()
-export class DirectMessageRoomService {}
+export class DirectMessageRoomService {
+  constructor(
+    private directRepository: DirectMessageRepository,
+    // private friendRepository: FriendRepository,
+    private directMessageRoomRepository: DirectMessageRoomRepository,
+  ) {}
+
+  @Transactional({ isolationLevel: IsolationLevel.REPEATABLE_READ })
+  async getAllDirectMessageRooms(
+    getDto: GetDirectMessageRoomsDto,
+  ): Promise<DirectMessageRoomsDto> {
+    const directMessageRooms: DirectMessageRoom[] =
+      await this.directMessageRoomRepository.findAllByUserId(getDto.userId);
+
+    const responseDto: DirectMessageRoomsDto = {
+      chatList: [],
+    };
+
+    for (const directMessageRoom of directMessageRooms) {
+      const directMessageRoomInfo: DirectMessageRoomInfoDto = {
+        nickname: directMessageRoom.friendId.nickname,
+        imgUrl: directMessageRoom.friendId.image.url,
+        newChat: await this.directRepository.countAllUnreadChatByRoomId(
+          directMessageRoom.roomId,
+          directMessageRoom.lastReadMessageId,
+        ),
+      };
+
+      responseDto.chatList.push(directMessageRoomInfo);
+    }
+
+    return responseDto;
+  }
+}
