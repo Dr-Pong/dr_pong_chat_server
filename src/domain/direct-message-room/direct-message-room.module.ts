2cf12964292391f225c5c2cc8cca11ab66daad9b
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-05 12:52:39 +0900

    [Fix] 종속성 DM 테스트 파일DMR에 추가 #24-1

--- src/domain/direct-message-room/direct-message-room.module.ts
+++ src/domain/direct-message-room/direct-message-room.module.ts
@@ -3,10 +3,16 @@ import { DirectMessageRoomService } from './direct-message-room.service';
 import { DirectMessageRoom } from './direct-message-room.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DirectMessageRoomRepository } from './direct-message-room.repository';
+import { DirectMessage } from '../direct-message/direct-message.entity';
+import { DirectMessageRepository } from '../direct-message/direct-message.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DirectMessageRoom])],
-  providers: [DirectMessageRoomService, DirectMessageRoomRepository],
+  imports: [TypeOrmModule.forFeature([DirectMessageRoom, DirectMessage])],
+  providers: [
+    DirectMessageRoomService,
+    DirectMessageRoomRepository,
+    DirectMessageRepository,
+  ],
   exports: [DirectMessageRoomService],
   controllers: [],
 })
