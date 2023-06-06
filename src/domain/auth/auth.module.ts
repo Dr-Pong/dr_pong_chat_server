7075e3d4ea697bc4f4004949bb635f8723dc6974
nheo9143 <nheo@student.42seoul.kr>
2023-06-06 14:56:33 +0900

    [Feat] jwt strategy, user decotator 추가 #56

---
+++ src/domain/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt/auth.jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'jwtSecret',
+      signOptions: {
+        expiresIn: 60 * 60 * 60,
+      },
+    }),
+  ],
+  providers: [JwtStrategy],
+})
+export class AuthModule {}
