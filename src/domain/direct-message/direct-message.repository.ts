cb0f6de3f0adac82ed91fb9a872c756ee42ef054
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-06 12:39:34 +0900

    [Feat] Repository함수 주석추가 #52

--- src/domain/direct-message/direct-message.repository.ts
+++ src/domain/direct-message/direct-message.repository.ts
@@ -11,6 +11,14 @@ export class DirectMessageRepository {
     private readonly repository: Repository<DirectMessage>,
   ) {}
 
+  /**
+   * 주어진 사용자 ID와 친구 ID에 해당하는 대화 메시지를 조회합니다.
+   * @param userId - 사용자 ID
+   * @param friendId - 친구 ID
+   * @param offset - 조회 시작 위치 (오프셋)
+   * @param count - 조회할 메시지 수
+   * @returns Promise<DirectMessage[]> - 조회된 대화 메시지 배열을 반환합니다.
+   */
   async findAllByUserIdAndFriendId(
     userId: number,
     friendId: number,
@@ -30,6 +38,13 @@ export class DirectMessageRepository {
     return directMessages;
   }
 
+  /**
+   * 주어진 사용자 ID, 친구 ID, 메시지를 저장합니다.
+   * @param userId - 보내는 사용자의 ID
+   * @param friendId - 메시지를 받을 친구의 ID
+   * @param message - 전송할 메시지 내용
+   * @returns Promise<void> - Promise를 반환하며, 아무 값도 반환하지 않습니다.
+   */
   async save(userId: number, friendId: number, message: string): Promise<void> {
     const directMessage: DirectMessage = await this.repository.create({
       sender: { id: userId },
@@ -43,6 +58,12 @@ export class DirectMessageRepository {
     await this.repository.save(directMessage);
   }
 
+  /**
+   * 주어진 사용자 ID, 친구 ID에 해당하는 대화 메시지 중 가장 최근 메시지를 조회합니다.
+   * @param userId - 사용자 ID
+   * @param friendId - 친구 ID
+   * @returns Promise<DirectMessage> - 조회된 가장 최근 메시지를 반환합니다.
+   */
   async findByUserIdAndFriendIdOrderByIdDesc(
     userId: number,
     friendId: number,
@@ -61,6 +82,13 @@ export class DirectMessageRepository {
     return directMessage;
   }
 
+  /**
+   * 주어진 대화방 ID와 마지막 메시지 ID보다 큰 ID를 가지는 대화 메시지의 수를 카운트합니다.
+   *
+   * @param roomId - 대화방 ID
+   * @param lastMessageId - 마지막 메시지 ID
+   * @returns Promise<number> - 읽지 않은 대화 메시지 수를 반환합니다.
+   */
   async countAllUnreadChatByRoomId(
     roomId: string,
     lastMessageId: number,
@@ -76,6 +104,13 @@ export class DirectMessageRepository {
     return unreadChats;
   }
 
+  /**
+   * 주어진 대화방 ID와 마지막 메시지 ID보다 큰 ID를 가지는 대화 메시지가 있는지 확인합니다.
+   *
+   * @param roomId - 대화방 ID
+   * @param lastMessageId - 마지막 메시지 ID
+   * @returns Promise<boolean> - 대화 메시지가 있는 경우 true를 반환하고, 그렇지 않은 경우 false를 반환합니다.
+   */
   async hasAnyUnreadChatByRoomId(
     roomId: string,
     lastMessageId: number,
