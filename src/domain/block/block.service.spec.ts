8dd7bb61aa696d48250781220f81ac427ee67363
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-06 19:33:48 +0900

    [Feat] 차단목록 불러오는 로직 수정 유저 차단 Post 테스트 로직 생성 #59

--- src/domain/block/block.service.spec.ts
+++ src/domain/block/block.service.spec.ts
@@ -13,12 +13,17 @@ import { TestModule } from './test/block.test.module';
 import { BlockModule } from './block.module';
 import { GetUserBlocksDto } from './dto/get.user.blocks.dto';
 import { UserBlocksDto } from './dto/user.blocks.dto';
+import { UserModel } from '../factory/model/user.model';
+import { UserFactory } from '../factory/user.factory';
+import { PostUserBlockDto } from './dto/post.user.block.dto';
+import { BadRequestException } from '@nestjs/common';
 
 describe('BlockService', () => {
   let service: BlockService;
   let testData: BlockTestService;
   let dataSources: DataSource;
   let blockRepository: Repository<Block>;
+  let userFactory: UserFactory;
 
   beforeAll(async () => {
     initializeTransactionalContext();
@@ -52,16 +57,12 @@ describe('BlockService', () => {
     testData = module.get<BlockTestService>(BlockTestService);
     dataSources = module.get<DataSource>(DataSource);
     blockRepository = module.get<Repository<Block>>(getRepositoryToken(Block));
-  });
-
-  beforeEach(async () => {
-    await testData.createProfileImages();
-    await testData.createBasicUsers(100);
+    userFactory = module.get<UserFactory>(UserFactory);
   });
 
   afterEach(async () => {
-    testData.clear();
-    jest.resetAllMocks();
+    userFactory.users.clear();
+    // jest.resetAllMocks();
     await dataSources.synchronize(true);
   });
 
@@ -70,12 +71,14 @@ describe('BlockService', () => {
     await dataSources.destroy();
   });
   describe('차단관련 Service Logic', () => {
-    describe('차단목록 조회', () => {
-      it('[Valid Case] 차단형식 확인', async () => {
-        await testData.createUserBlocks(10);
+    describe('GET 차단목록 조회', () => {
+      it('[Valid Case] 차단형식 확인 차단유저가 있는경우', async () => {
+        const user1: UserModel = await testData.createBasicUser('user1');
+        const user2: UserModel = await testData.createBasicUser('user2');
+        await testData.createUserBlocks(user1, user2);
 
         const blockListDto: GetUserBlocksDto = {
-          userId: testData.users[0].id,
+          userId: user1.id,
         };
 
         const blockList: UserBlocksDto = await service.getUserBlocks(
@@ -85,29 +88,14 @@ describe('BlockService', () => {
         expect(blockList).toHaveProperty('users');
         expect(blockList.users[0]).toHaveProperty('nickname');
         expect(blockList.users[0]).toHaveProperty('imgUrl');
-      });
 
-      it('[Valid Case] 차단유저가 있는경우 ', async () => {
-        await testData.createUserBlocks(10);
-
-        const blockListDto: GetUserBlocksDto = {
-          userId: testData.users[0].id,
-        };
-
-        const blockList: UserBlocksDto = await service.getUserBlocks(
-          blockListDto,
-        );
-
-        expect(blockList.users.length).toBe(10);
-        expect(blockList.users[0].nickname).toBe(testData.users[1].nickname);
-        expect(blockList.users[0].imgUrl).toBe(testData.users[1].image.url);
-        expect(blockList.users[9].nickname).toBe(testData.users[10].nickname);
-        expect(blockList.users[9].imgUrl).toBe(testData.users[10].image.url);
+        expect(blockList.users[0].nickname).toBe('user2');
       });
 
       it('[Valid Case] 차단유저가 빈경우', async () => {
+        const user: UserModel = await testData.createBasicUser('user1');
         const blockListDto: GetUserBlocksDto = {
-          userId: testData.users[0].id,
+          userId: user.id,
         };
 
         const blockList: UserBlocksDto = await service.getUserBlocks(
@@ -117,15 +105,76 @@ describe('BlockService', () => {
         expect(blockList.users.length).toBe(0);
       });
     });
-    describe('유저 차단', () => {
-      it('[Valid Case] 유저가 차단되는지', async () => {});
-      it('[Valid Case] 이전에 차단 목록에 차단할 유저가 있을때', async () => {});
-      it('[Valid Case] 중복 차단 요청시 차단유지 및 백에서 씹는지', async () => {});
-      it('[Error Case]  존재하지 않는 유저에 대한 차단 요청시 적절한 에러 응답', async () => {});
+    describe('POST 유저 차단', () => {
+      it('[Valid Case] 유저가 차단되는지', async () => {
+        const user1: UserModel = await testData.createBasicUser('user1');
+        const user2: UserModel = await testData.createBasicUser('user2');
+        const userBlockDto: PostUserBlockDto = {
+          userId: user1.id,
+          friendId: user2.id,
+        };
+
+        await service.postUserBlocks(userBlockDto);
+
+        const BlocksDb: Block = await blockRepository.findOne({
+          where: {
+            user: { id: user1.id },
+            blockedUser: { id: user2.id },
+          },
+        });
+        expect(BlocksDb).toBe(1);
+        expect(BlocksDb.user.id).toBe(user1.id);
+        expect(BlocksDb.blockedUser.id).toBe(user2.id);
+
+        const UserFt: UserModel = userFactory.findById(user1.id);
+        expect(UserFt.blockedList.size).toBe(1);
+        expect(UserFt.blockedList.has(user2.id)).toBe(true);
+      });
+
+      it('[Valid Case] 이전에 차단 목록에 차단할 유저가 있을때', async () => {
+        const user1: UserModel = await testData.createBasicUser('user1');
+        const user2: UserModel = await testData.createBasicUser('user2');
+        await testData.createUserBlocks(user1, user2);
+
+        const userBlockDto: PostUserBlockDto = {
+          userId: user1.id,
+          friendId: user2.id,
+        };
+
+        await service.postUserBlocks(userBlockDto);
+
+        const BlocksDb: Block = await blockRepository.findOne({
+          where: {
+            user: { id: user1.id },
+            blockedUser: { id: user2.id },
+          },
+        });
+        expect(BlocksDb).toBe(1);
+        expect(BlocksDb.user.id).toBe(user1.id);
+        expect(BlocksDb.blockedUser.id).toBe(user2.id);
+
+        const UserFt: UserModel = userFactory.findById(user1.id);
+        expect(UserFt.blockedList.size).toBe(1);
+        expect(UserFt.blockedList.has(user2.id)).toBe(true);
+      });
+
+      it('[Error Case]  존재하지 않는 유저에 대한 차단 요청시 적절한 에러 응답', async () => {
+        const user1: UserModel = await testData.createBasicUser('user1');
+        const userBlockDto: PostUserBlockDto = {
+          userId: user1.id,
+          friendId: 100,
+        };
+
+        await expect(service.postUserBlocks(userBlockDto)).rejects.toThrowError(
+          new BadRequestException('Invalid userId'),
+        );
+      });
     });
-    describe('차단 해제', () => {
+    describe('DELETE 차단 해제', () => {
       it('[Valid Case] 차단이 해제되는지', async () => {});
+
       it('[Valid Case] 차단해제후 차단목록에 없는지 조회', async () => {});
+
       it('[Error Case] 차단 목록에 없는 유저를 해제하려할때 에러응답', async () => {});
     });
   });
