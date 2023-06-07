153ca68f7a664d745e0849da0dc95de996834f90
triplecheeseburger <hangyulkim94@gmail.com>
2023-06-07 15:37:22 +0900

    [feat] user repository 생성 #78

---
+++ src/domain/user/user.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: Repository<User>,
+  ) {}
+  async findByNickname(nickname: string): Promise<User> {
+    return await this.repository.findOne({
+      where: { nickname: nickname },
+    });
+  }
+}
