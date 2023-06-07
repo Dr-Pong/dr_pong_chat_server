ab224b7d894dd3ead887247baa629a5e58902c6d
triplecheeseburger <hangyulkim94@gmail.com>
2023-06-07 15:55:01 +0900

    [fix] 이무슨리포지토리? #78

--- src/domain/user/test/user.test.module.ts
+++ src/domain/user/test/user.test.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user.entity';
 import { UserTestService } from './user.test.service';
-import { ProfileImageRepository } from '../../profile-image/profile-image.repository';
-import {ProfileImage} from "../../profile-image/profile-image.entity";
+import { ProfileImage } from '../../profile-image/profile-image.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, ProfileImage, ProfileImageRepository])],
+  imports: [TypeOrmModule.forFeature([User, ProfileImage])],
   providers: [UserTestService],
   exports: [UserTestService],
 })
