6137da7faf83b845382b8269df27dbcb5e6c16a9
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-08 15:41:05 +0900

    [Fix] dto type추가 #94

--- src/domain/direct-message/dto/get.direct-message.history.response.dto.ts
+++ src/domain/direct-message/dto/get.direct-message.history.response.dto.ts
@@ -1,8 +1,11 @@
+import { ChatType } from 'src/global/type/type.chat';
+
 export class ChatDto {
   id: number;
   nickname: string;
   message: string;
   time: Date;
+  type: ChatType;
 }
 
 export class GetDirectMessageHistoryResponseDto {
--- src/domain/friend/dto/friend.direct.message.chat.list.response.dto.ts
+++ src/domain/friend/dto/friend.direct.message.chat.list.response.dto.ts
@@ -1,8 +1,11 @@
+import { ChatType } from 'src/global/type/type.chat';
+
 interface Chat {
   id: number;
   message: string;
   nickname: string;
   time: Date;
+  type: ChatType;
 }
 
 export class FriendDirectMessageChatListResponseDto {
