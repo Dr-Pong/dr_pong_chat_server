86e1dba9745384cc39e691ffd9acec3c02f15435
nheo9143 <nheo@student.42seoul.kr>
2023-06-13 14:02:55 +0900

    [Feat] user realtion 서비스 구현 #131

--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -3,10 +3,18 @@ import { GetIdFromNicknameDto } from './dto/get.id.from.nickname.dto';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserIdDto } from './dto/user.id.dto';
 import { UserRepository } from './user.repository';
+import { FriendRepository } from '../friend/friend.repository';
+import { BlockRepository } from '../block/block.repository';
+import { GetUserRelationDto } from './dto/get.user.relation.dto';
+import { UserRelationDto } from './dto/user.relation.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly friendRepository: FriendRepository,
+    private readonly blockRepository: BlockRepository,
+  ) {}
 
   @Transactional({ isolationLevel: IsolationLevel.REPEATABLE_READ })
   async getIdFromNickname(getDto: GetIdFromNicknameDto): Promise<UserIdDto> {
@@ -14,5 +22,21 @@ export class UserService {
     const user = await this.userRepository.findByNickname(nickname);
     if (!user) throw new BadRequestException('No such User');
     return { id: user.id };
+  }
+
+  @Transactional({ isolationLevel: IsolationLevel.REPEATABLE_READ })
+  async getUserRelation(getDto: GetUserRelationDto): Promise<UserRelationDto> {
+    const { userId, targetId } = getDto;
+    const isFriend: boolean =
+      await this.friendRepository.checkIsFriendByUserIdAndFriendId(
+        userId,
+        targetId,
+      );
+    const isBlock: boolean =
+      await this.blockRepository.checkIsBlockByUserIdAndTargetId(
+        userId,
+        targetId,
+      );
+    return new UserRelationDto(isFriend, isBlock);
   }
 }
