d3bebdc9c76a1783f64483be6df5e717d23d6c25
triplecheeseburger <hangyulkim94@gmail.com>
2023-06-14 16:33:03 +0900

    [fix] 필요없는 의존성 제거 #153

--- src/domain/notification/notification.module.ts
+++ src/domain/notification/notification.module.ts
@@ -1,14 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../user/user.entity';
-import { Friend } from '../friend/friend.entity';
-import { Block } from '../block/block.entity';
-import { FactoryModule } from '../factory/factory.module';
-import { UserService } from '../user/user.service';
-import { UserRepository } from '../user/user.repository';
-import { FriendRepository } from '../friend/friend.repository';
-import { BlockRepository } from '../block/block.repository';
-import { UserController } from '../user/user.controller';
 import { NotificationController } from './notification.controller';
 import { ChannelModule } from '../channel/channel.module';
 import { FriendModule } from '../friend/friend.module';
