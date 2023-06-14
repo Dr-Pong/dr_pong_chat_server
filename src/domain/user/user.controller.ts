17a884570258499c0708f9223f7ab4e2116ebbfd
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-14 12:53:06 +0900

    [Feat] 프로필 이미지 patch 컨트롤러 구현 #149

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRelationResponseDto } from './dto/user.relation.response.dto';
 import { PostGatewayUserDto } from './dto/post.gateway.users.dto';
+import { Requestor } from '../auth/jwt/auth.requestor.decorator';
+import { UserIdCardDto } from '../auth/jwt/auth.user.id-card.dto';
+import { PatchUserImageRequestDto } from './dto/patch.user.image.request.dto';
+import { PatchUserImageDto } from './dto/patch.user.image.dto';
 
 @Controller('users')
 export class UserController {
@@ -32,5 +36,17 @@ export class UserController {
   @Post('/')
   async userPost(@Body() postDto: PostGatewayUserDto): Promise<void> {
     await this.userService.postUser(postDto);
+  }
+
+  @Patch('/:nickname/image')
+  async usersImageByNicknamePatch(
+    @Requestor() requestor: UserIdCardDto,
+    @Param('nickname') nickname: string,
+    @Body()
+    patchRequestDto: PatchUserImageRequestDto,
+  ): Promise<void> {
+    const patchUserImageDto: PatchUserImageDto =
+      PatchUserImageDto.forPatchUserImageDto(requestor, patchRequestDto);
+    await this.userService.patchUserImage(patchUserImageDto);
   }
 }
