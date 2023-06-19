c4a54d75796840ecaea41851f7ab1dd456159805
nheo9143 <nheo@student.42seoul.kr>
2023-06-19 19:09:29 +0900

    [Feat] 프로필 이미지 init해주는 부분 추가 #173

--- src/app.service.ts
+++ src/app.service.ts
@@ -52,6 +52,12 @@ export class AppService implements OnApplicationBootstrap {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
+        {
+          id: 4,
+          url: 'https://42gg-public-image.s3.ap-northeast-2.amazonaws.com/images/hakim.jpeg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
       );
     }
     const channels: Channel[] = await this.channelRepository.findAll();
