dd56c57f1718eb7e5e221a45cdd5b766d0caa6bd
nheo9143 <nheo@student.42seoul.kr>
2023-07-17 21:16:43 +0900

    [Refactor] 친구 알림 dto들 경로 변경 #205

--- src/domain/notification/notification.module.ts
+++ src/domain/notification/notification.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { ChannelModule } from '../channel/channel.module';
 import { FriendModule } from '../friend/friend.module';
+import { InvitationModule } from '../invitation/invitation.module';
 
 @Module({
-  imports: [FriendModule, ChannelModule],
+  imports: [FriendModule, ChannelModule, InvitationModule],
   providers: [],
   exports: [],
   controllers: [NotificationController],
