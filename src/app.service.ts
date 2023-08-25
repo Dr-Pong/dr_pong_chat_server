f1658ee2b9cef4dfa216c59583fdae7e28928107
nheo9143 <nheo@student.42seoul.kr>
2023-08-25 17:40:24 +0900

    [Fix] 기본 프로필 이미지 수정

--- src/app.service.ts
+++ src/app.service.ts
@@ -33,50 +33,17 @@ export class AppService implements OnApplicationBootstrap {
   async onApplicationBootstrap(): Promise<void> {
     const images: ProfileImage[] = await this.imageRepository.findAll();
     if (images.length === 0) {
-      await this.imageRepository.save(
-        {
-          id: 1,
-          url: 'https://drpong.s3.ap-northeast-2.amazonaws.com/developers/hakim.png',
+      for (let i = 1; i <= 8; i++) {
+        await this.imageRepository.save({
+          id: i,
+          url:
+            'https://drpong.s3.ap-northeast-2.amazonaws.com/fishes/' +
+            i.toString() +
+            '.jpg',
           createdAt: new Date(),
           updatedAt: new Date(),
-        },
-        {
-          id: 2,
-          url: 'https://drpong.s3.ap-northeast-2.amazonaws.com/developers/jiyun.png',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          id: 3,
-          url: 'https://drpong.s3.ap-northeast-2.amazonaws.com/developers/jihyukim.png',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          id: 4,
-          url: 'https://drpong.s3.ap-northeast-2.amazonaws.com/developers/jaehwkim.png',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          id: 5,
-          url: 'https://drpong.s3.ap-northeast-2.amazonaws.com/developers/junyopar.png',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          id: 6,
-          url: 'https://drpong.s3.ap-northeast-2.amazonaws.com/developers/keokim.png',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          id: 7,
-          url: 'https://drpong.s3.ap-northeast-2.amazonaws.com/developers/nheo.png',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      );
+        });
+      }
     }
     const channels: Channel[] = await this.channelRepository.findAll();
     const users: User[] = await this.userRepository.findAll();
